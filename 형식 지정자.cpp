5cc6070c3ad07ec5294351209d0ff84c02db8f5f
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-06-22 16:28:55 +0900

    Add files via upload

---
+++ 형식 지정자.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("%d \n", 10);
+	printf("%f \n", 3.14);
+	printf("%c \n", 'a');
+	printf("%s \n", "Hello");
+	printf("%d %f", 10, 3.578);
+
+	return 0;
+}
