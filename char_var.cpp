55fbc1f44e5d62351a696eb92d30157be086c8d7
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-07-18 20:34:24 +0900

    Add files via upload

---
+++ char_var.cpp
@@ -0,0 +1,16 @@
+// 문자 변수와 문자 상수
+#include <stdio.h>
+
+int main(void)
+{
+	char code1 = 'A';
+	char code2 = 65;
+
+	printf("code1 = %c\n", code1);
+	printf("code2 = %c\n", code2);
+
+	printf("code1 = %d\n", code1);
+	printf("code2 = %d\n", code2);
+
+	return 0;
+}
