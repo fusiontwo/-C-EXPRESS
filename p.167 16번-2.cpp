37d3374fee813b831df62014f9175c90acf6827d
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-07-19 15:25:42 +0900

    Add files via upload

---
+++ p.167 16번-2.cpp
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int a = 100;
+	char b = 'X';
+	float c = 1.2345;
+
+	printf("\a");
+	printf("예제\t프로그램\n");
+	printf("%d, %c, %f\n", a, b, c);
+	return 0;
+}
