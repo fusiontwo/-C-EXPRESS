ad75fd3c83b8d48094e75ca6464aaf54751e0adf
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-07-23 19:52:21 +0900

    Add files via upload

---
+++ while syntax.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int i = 0;
+
+	while (i < 5)  // 반복 조건
+	{
+		printf("Hello World! \n");
+		i++;
+	}                              // 반복 내용(블록)
+
+	return 0;
+}
