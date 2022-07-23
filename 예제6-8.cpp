f03a69429831059ff08cb61a826891814085e72e
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-07-23 18:45:59 +0900

    Add files via upload

---
+++ 예제6-8.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int time, age;
+	printf("현재 시간과 나이를 입력하시오(시간, 나이): ");
+	scanf("%d %d", &time, &age);
+
+	if (time < 17)
+		if ((age >= 3 && age <= 12) || age >= 65)
+			printf("요금은 25,000입니다.");
+		else
+			printf("요금은 34,000입니다.");
+	else
+		printf("요금은 10,000입니다.");
+	return 0;
+}
