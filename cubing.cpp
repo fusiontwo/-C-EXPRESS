ab8b79df31b36eef6a8de7b307cb63e0c4c79f11
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-07-25 15:11:02 +0900

    Add files via upload

---
+++ cubing.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int n;
+
+	printf("정수를 입력하시요: ");
+	scanf("%d", &n);
+
+	printf("====================\n");
+	printf("   i     i의 세제곱\n");
+	printf("====================\n");
+	for (int i = 1; i <= n; i++)
+		printf("%5d %5d\n", i, i * i * i);
+
+	return 0;
+}
