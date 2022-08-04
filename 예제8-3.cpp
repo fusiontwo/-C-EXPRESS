254bb8473d2c7f66c9edb9a8f9fa57d15ca0659a
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-08-04 12:41:11 +0900

    Add files via upload

---
+++ 예제8-3.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+# define PI 3.141592
+
+double cal_area(double radius);
+
+int main(void)
+{
+	double radius;
+
+	printf("원의 반지름을 입력하시오: ");
+	scanf("%lf", &radius);
+
+	printf("원의 면적은 %lf입니다.", cal_area(radius));
+	return 0;
+}
+
+double cal_area(double radius)
+{
+	double area;
+	area = PI * radius * radius;
+	return area;
+}
