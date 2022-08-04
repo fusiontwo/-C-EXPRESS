9f2f72f5cc30aba70c24f3de9aa16b2e8bd7366d
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-08-04 15:05:08 +0900

    Add files via upload

---
+++ 예제8-6 조건연산자 사용.cpp
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <math.h>
+
+int even(int n);
+int absolute(int n);
+int sign(int n);
+
+int main(void)
+{
+	int n;
+	printf("정수를 입력하시오: ");
+	scanf("%d", &n);
+
+	if (even(n) == 1)
+		printf("even()의 결과: 짝수\n");
+	else
+		printf("even()의 결과: 홀수\n");
+
+	printf("absolute()의 결과: %d\n", absolute(n));
+
+	if (sign(n) == -1)
+		printf("sign()의 결과: 음수\n");
+	else if (sign(n) == 0)
+		printf("sign()의 결과: 0\n");
+	else
+		printf("sign()의 결과: 양수\n");
+}
+
+int even(int n)
+{
+	return (n % 2 == 0) ? 1 : 0;
+}
+
+int absolute(int n)
+{
+	return (n < 0) ? n * (-1) : n;
+}
+
+int sign(int n)
+{
+	if (n < 0)
+		return -1;
+	else if (n == 0)
+		return 0;
+	else
+		return 1;
+}
