0f63e2135d1e0dc0a30830182b43095fa1155f67
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-08-06 19:36:22 +0900

    Add files via upload

--- gcd.cpp
+++ gcd.cpp
@@ -1,20 +1,18 @@
-// while 문을 이용한 최대 공약수 구하기 프로그램
+// 최대 공약수 구하기
 #include <stdio.h>
+
+int gcd(int x, int y);
 
 int main(void)
 {
-	int x, y, r;
+	printf("%d\n", gcd(30, 20));
+}
 
-	printf("두개의 정수를 입력하시오(큰수, 작은수): ");
-	scanf("%d%d", &x, &y);
-
-	while (y != 0)
-	{
-		r = x % y;
-		x = y;
-		y = r;
-	}
-	printf("최대 공약수는 %d입니다.\n", x);
-
-	return 0;
+// x가 y보다 커야 한다.
+int gcd(int x, int y)
+{
+	if (y == 0)
+		return x;
+	else
+		return gcd(y, x % y);
 }
