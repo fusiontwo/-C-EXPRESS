80293cd89df7ca92535ad20cb115c32125ef2b0d
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-08-08 21:38:20 +0900

    Add files via upload

---
+++ 예제9-6.cpp
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int power(int base, int power_raised);
+int main(void)
+{
+	int base, power_raised;
+	printf("¹Ø¼ö: ");
+	scanf("%d", &base);
+	printf("Áö¼ö: ");
+	scanf("%d", &power_raised);
+	printf("%d^%d = %d", base, power_raised, power(base, power_raised));
+}
+int power(int base, int power_raised)
+{
+	if (power_raised == 0)
+		return 1;
+	else
+		return base * power(base, power_raised - 1);
+}
