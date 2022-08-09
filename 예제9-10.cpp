7f272d0cf6ab256f3f527966b2a7972ec09e88ed
fusiontwo <101881124+fusiontwo@users.noreply.github.com>
2022-08-09 14:30:08 +0900

    Add files via upload

---
+++ 예제9-10.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+double recursive(double n);
+int main(void)
+{
+	double n, result;
+	
+	printf("n의 값을 입력하세요: ");
+	scanf("%lf", &n);
+
+	result = recursive(n);
+	printf("%f", result);
+
+	return 0;
+}
+
+double recursive(double n)
+{
+	if (n == 1)
+		return 1;
+	else
+		return ((1 / n) + recursive(n - 1));
+}
